6680fbbe306e02031714e645bbedc863399c71c0
Marie <marie.clerc@laplateforme.io>
2024-07-12 14:42:18 +0200

    observable du btn PLUS et MOINS mais ils sont indepandant, et non liés pour le moment

--- src/app/webApp/accueil/home/home.component.ts
+++ src/app/webApp/accueil/home/home.component.ts
@@ -19,40 +19,95 @@ export class HomeComponent implements OnInit, AfterViewInit {
 
 
   @ViewChild('cours') eltUl!: ElementRef<HTMLElement>;
+  // props
+  @ViewChild('plus') eltPlus!: ElementRef<HTMLElement>;
+  @ViewChild('moins') eltMoins!: ElementRef<HTMLElement>;
+  @ViewChild('panier') eltPanier!: ElementRef<HTMLElement>;
 
   ngOnInit(): void {
     const array: string[] = ['Angular 18', 'React 18', 'Vue', 'NextJS', 'Vite'];
     // (array) on recupère un Obs
-    // this.infos$ = from(array)
-    // .pipe(
-    //   delay(1500),
-    //   tap(
-    //     (formation:string) => {
-    //       console.log(formation);
-
-    //     }
-    //   )
-    // );
-
     this.infos$ = from(array)
-      .pipe(
-        concatMap(
-          // of envoie les formation les une apres les autres, mais 
-          // est aussi un Obs
-          formation => of(formation)
-            .pipe(
-              delay(1000)
-            )
-        ),
-        tap(
-          (formation: string) => {
-            console.log(formation);
-          }
-        )
-      );
+    .pipe(
+      delay(1500),
+      tap(
+        (formation:string) => {
+          console.log(formation);
+
+        }
+      )
+    );
+
+    // this.infos$ = from(array)
+    //   .pipe(
+    //     concatMap(
+    //       // of envoie les formation les une apres les autres, mais 
+    //       // est aussi un Obs
+    //       formation => of(formation)
+    //         .pipe(
+    //           delay(1000)
+    //         )
+    //     ),
+    //     tap(
+    //       (formation: string) => {
+    //         console.log(formation);
+    //       }
+    //     )
+    //   );
   }
 
   ngAfterViewInit(): void {
 
+    // --------------------
+    // creation de l'Observable #1 : btn PLUS
+    // --------------------
+    const plus$ = fromEvent(this.eltPlus.nativeElement, 'click')
+    .pipe(
+      startWith(0),
+      // permet de transformer les data
+      map(
+        () => {
+          return +1
+        }
+      ), 
+      // maintenant on veut cumuler les valeurs
+      scan(
+        (accu:number, newval:number) => {
+          return accu + newval
+        }
+      )
+    )
+    .subscribe(
+      (valObs) => {
+        console.log(valObs);
+        this.eltPanier.nativeElement.innerHTML=`${valObs} article(s) en panier`
+      }
+    )
+
+    // -------------------------------------
+    // le button MOINS
+    
+    const moins$ = fromEvent(this.eltMoins.nativeElement, 'click')
+    .pipe(
+      startWith(0),
+      // permet de transformer les data
+      map(
+        () => {
+          return -1
+        }
+      ), 
+      // maintenant on veut cumuler les valeurs
+      scan(
+        (accu:number, newval:number) => {
+          return accu + newval
+        }
+      )
+    )
+    .subscribe(
+      (valObs) => {
+        console.log(valObs);
+        this.eltPanier.nativeElement.innerHTML=`${valObs} article(s) en panier`
+      }
+    )
   }
 }
