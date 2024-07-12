f301c5e9f1cb773bda620958c2a865eec6f90537
Marie <marie.clerc@laplateforme.io>
2024-07-12 14:29:34 +0200

    créer des fonction d'observables et manipuler les data émisent, on utlise les fonctions RxJS

--- src/app/webApp/accueil/home/home.component.ts
+++ src/app/webApp/accueil/home/home.component.ts
@@ -14,12 +14,42 @@ import { Observable, combineLatest, concatMap, delay, from, fromEvent, map, of, 
 export class HomeComponent implements OnInit, AfterViewInit {
 
   // public infos$:Observable<string> = new Observable();
+  // meme chose, mais synthaxe différente
   public infos$: Observable<string> = {} as Observable<string>;
+
+
   @ViewChild('cours') eltUl!: ElementRef<HTMLElement>;
 
   ngOnInit(): void {
-        const array: string[] = ['Angular 18', 'React 18', 'Vue', 'NextJS', 'Vite'];
-    
+    const array: string[] = ['Angular 18', 'React 18', 'Vue', 'NextJS', 'Vite'];
+    // (array) on recupère un Obs
+    // this.infos$ = from(array)
+    // .pipe(
+    //   delay(1500),
+    //   tap(
+    //     (formation:string) => {
+    //       console.log(formation);
+
+    //     }
+    //   )
+    // );
+
+    this.infos$ = from(array)
+      .pipe(
+        concatMap(
+          // of envoie les formation les une apres les autres, mais 
+          // est aussi un Obs
+          formation => of(formation)
+            .pipe(
+              delay(1000)
+            )
+        ),
+        tap(
+          (formation: string) => {
+            console.log(formation);
+          }
+        )
+      );
   }
 
   ngAfterViewInit(): void {
